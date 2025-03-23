fc07d9b70826c23f4561b353cbcb187789dbd482
pradnya2001 <npradnya8@gmail.com>
2025-03-23 23:21:01 +0530

    Create hollow rectangle in java

---
+++ hollowrectangle.java
@@ -0,0 +1,18 @@
+public class hollowrectangle {
+    
+    public static void main(String[] args){
+        int rows = 4;
+        int col = 5;
+        for(int i = 1; i<=rows;i++){
+            for(int j=1;j<=col;j++){
+                if(i==1 || j==1 || i==rows || j==col){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
